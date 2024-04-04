b8b39570516659c50282793bc1632cbb578ef384
Manthan Gajera <202301488@daiict.ac.in>
2024-04-05 00:23:31 +0530

    Update Linked_list.h

--- Linked_list.h
+++ Linked_list.h
@@ -3,14 +3,13 @@
 #include<ctime>
 using namespace std;
 struct node {
-	string folder;
 	string name;
 	time_t created_date;
 	int access_count;
 	node* next;
 
 	node(const string& folder, const string& name, time_t created_date, int access_count)
-		: folder(folder), name(name), created_date(created_date), access_count(access_count), next(nullptr) {}
+		:  name(name), created_date(created_date), access_count(access_count), next(nullptr) {}
 };
 
 typedef class Linkedlist {
